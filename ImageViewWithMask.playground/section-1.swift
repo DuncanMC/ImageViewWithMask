2779341506089def10137d303f8f7236cd0b5799
Duncan Champney <DuncanC@aol.com>
2015-04-15 10:23:45 -0400

    Added code comments and resructured slightly

--- ImageViewWithMask.playground/section-1.swift
+++ ImageViewWithMask.playground/section-1.swift
@@ -1,9 +1,16 @@
 import UIKit
 import AVFoundation
 
+/**
+  ImageViewWithGradient: A custom subclass of UIImageView that adds a mask layer on top of the image with a circular cutout in the middle
 
+  :param: inset: set inset to a non-zero value to inset the circular cutout
+  :param: image: inherited from UIImageView.
+*/
 class ImageViewWithGradient: UIImageView
 {
+  
+  //Changing this value invokes layoutSubviews to update the shape layer's path
   var inset: CGFloat = 0
     {
     didSet
@@ -28,16 +35,24 @@ class ImageViewWithGradient: UIImageView
   
   func setup()
   {
-    self.layer.addSublayer(myShapeLayer)
+    //Do onetime setup of the shape layer.
     myShapeLayer.opaque = false
     myShapeLayer.fillColor = UIColor(white: 1, alpha: 0.8).CGColor
     myShapeLayer.fillRule = kCAFillRuleEvenOdd
     
-    //The line below creates a frame around the view. 
-    //Remove it if you don't want a frame
+    //The line below creates a frame around the view.
+    //TODO: Remove the line below if you don't want a frame around your image view.
     myShapeLayer.borderWidth = 1.0
+    
+    //add our shape layer as a sublayer of the image view's layer.
+    self.layer.addSublayer(myShapeLayer)
   }
   
+  /**
+  Whenever our layout changes, rebuild the path used by the shape layer. 
+
+  This method is also called if the view's inset property is changed.
+*/
   override func layoutSubviews()
   {
     var frame = self.layer.bounds
@@ -55,13 +70,21 @@ class ImageViewWithGradient: UIImageView
   }
 }
 
-//Global code to create an example image. Click on the "eyeball" to the right of 
-//the line below that assigns aView.image = anImage to see the resulting custom image.
-
-let anImage = UIImage(named: "Scampers 6685.jpg")
-if let anImage = anImage
+//------------------------------------------------------------
+/**
+  Global function to create an example image. Click on the "eyeball" to the right of the line below that assigns `aView.image = anImage` to see the resulting custom image.
+*/
+func createSampleImage()
 {
-  var aView = ImageViewWithGradient(frame: CGRect(x: 0, y: 0, width: anImage.size.width, height: anImage.size.height))
-  aView.inset = CGFloat(100.0)
-  aView.image = anImage
+  let anImage = UIImage(named: "Scampers 6685.jpg")
+  if let anImage = anImage
+  {
+    var aView = ImageViewWithGradient(frame: CGRect(x: 0, y: 0, width: anImage.size.width, height: anImage.size.height))
+    aView.inset = CGFloat(100.0)
+    aView.image = anImage
+  }
 }
+
+//This is playground-only code that invokes createSampleImage 
+//to create a sample ImageViewWithMask
+createSampleImage()
